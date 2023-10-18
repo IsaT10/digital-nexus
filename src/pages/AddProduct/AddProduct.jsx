e7d780f7bc291fd84798c18fda1d39fd83c55380
IsaT <rakiib010@gmail.com>
2023-10-18 12:14:38 +0600

    add product page complete

--- src/pages/AddProduct/AddProduct.jsx
+++ src/pages/AddProduct/AddProduct.jsx
@@ -1,82 +1,127 @@
 import { useState } from "react";
 import DescribeRoute from "../../components/DescribeRoute/DescribeRoute";
+import Button from "../../components/Button";
 
 const AddProduct = () => {
-  const [toggleLabel, setToogleLabel] = useState(false);
   const [productInfo, setProductInfo] = useState({
     name: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
+    brandName: "",
+    type: "",
+    price: "",
+    shortDescription: "",
+    rating: "",
+    image: "",
   });
 
-  const handleNameChange = (e) => {
-    setProductInfo({ ...productInfo, name: e.target.value });
-
-    handleToogle.call(productInfo);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const product = { ...productInfo };
+    console.log(product);
   };
-  const handleToogle = () => {
-    // if (productInfo.name.length === 0) {
-    //   setToogleLabel(true);
-    // } else if (productInfo.name.length > 0) {
-    //   setToogleLabel(true);
-    // }
-    console.log(productInfo.name);
-    console.log(productInfo.name.length);
-  };
-
-  //   console.log(productInfo.name.length);
 
   return (
     <div className="my-5 md:mt-10">
       <DescribeRoute>addproduct</DescribeRoute>
       <div className="border-t-2 border-stone-600">
-        <form className="max-w-7xl xl:mx-auto mx-10  my-12 flex flex-col gap-20">
+        <form
+          onSubmit={handleSubmit}
+          className="max-w-7xl xl:mx-auto mx-10  my-12 flex flex-col gap-20"
+        >
           <div className="flex gap-10">
             <div className="w-1/2">
-              {toggleLabel ? <label>ssd</label> : ""}
-              <input
-                className="bg-black border-b-[1px] border-stone-400 outline-none w-full pb-2 text-stone-200 pl-3 placeholder:text-stone-400"
-                type="text"
-                value={productInfo.name}
-                onKeyDown={handleNameChange}
-                placeholder="Your e-mail"
-              />
-            </div>
-            <div className="w-1/2">
-              {toggleLabel ? <label>ssd</label> : ""}
+              <label className="text-stone-400">Name</label>
 
               <input
-                className="bg-black border-b-[1px] border-stone-400 outline-none w-full pb-2 text-stone-200 pl-3 placeholder:text-stone-400"
+                className="bg-black border-b-[1px] border-stone-400 outline-none w-full pb-2 text-stone-300  placeholder:text-stone-400 placeholder:pl-3"
                 type="text"
-                placeholder="Your e-mail"
+                value={productInfo.name}
+                onChange={(e) => {
+                  setProductInfo({ ...productInfo, name: e.target.value });
+                }}
+                //
+              />
+            </div>
+            <div className="w-1/2">
+              <label className="text-stone-400">Brand name</label>
+
+              <input
+                className="bg-black border-b-[1px] border-stone-400 outline-none w-full pb-2 text-stone-300  placeholder:text-stone-400 placeholder:pl-3"
+                type="text"
+                value={productInfo.brandName}
+                onChange={(e) => {
+                  setProductInfo({ ...productInfo, brandName: e.target.value });
+                }}
               />
             </div>
           </div>
           <div className="flex gap-10">
-            <input
-              className="bg-black border-b-[1px] border-stone-400 outline-none w-full pb-2 text-stone-200 pl-3 placeholder:text-stone-400"
-              type="text"
-              placeholder="Your e-mail"
-            />
-            <input
-              className="bg-black border-b-[1px] border-stone-400 outline-none w-full pb-2 text-stone-200 pl-3 placeholder:text-stone-400"
-              type="text"
-              placeholder="Your e-mail"
-            />
+            <div className="w-1/2">
+              <label className="text-stone-400">Type</label>
+              <input
+                className="bg-black border-b-[1px] border-stone-400 outline-none w-full pb-2 text-stone-300  placeholder:text-stone-400 placeholder:pl-3"
+                type="text"
+                value={productInfo.type}
+                onChange={(e) => {
+                  setProductInfo({ ...productInfo, type: e.target.value });
+                }}
+              />
+            </div>
+            <div className="w-1/2">
+              <label className="text-stone-400">Price</label>
+
+              <input
+                className="bg-black border-b-[1px] border-stone-400 outline-none w-full pb-2 text-stone-300  placeholder:text-stone-400 placeholder:pl-3"
+                value={productInfo.price}
+                onChange={(e) => {
+                  setProductInfo({ ...productInfo, price: e.target.value });
+                }}
+                type="text"
+              />
+            </div>
           </div>
           <div className="flex gap-10">
+            <div className="w-1/2">
+              <label className="text-stone-400">Short description</label>
+              <input
+                className="bg-black border-b-[1px] border-stone-400 outline-none w-full pb-2 text-stone-300  placeholder:text-stone-400 placeholder:pl-3"
+                type="text"
+                value={productInfo.shortDescription}
+                onChange={(e) => {
+                  setProductInfo({
+                    ...productInfo,
+                    shortDescription: e.target.value,
+                  });
+                }}
+              />
+            </div>
+            <div className="w-1/2">
+              <label className="text-stone-400">Rating</label>
+
+              <input
+                className="bg-black border-b-[1px] border-stone-400 outline-none w-full pb-2 text-stone-300  placeholder:text-stone-400 placeholder:pl-3"
+                value={productInfo.rating}
+                onChange={(e) => {
+                  setProductInfo({ ...productInfo, rating: e.target.value });
+                }}
+                type="text"
+              />
+            </div>
+          </div>
+          <div className="w-full">
+            <label className="text-stone-400">Image URL</label>
             <input
-              className="bg-black border-b-[1px] border-stone-400 outline-none w-full pb-2 text-stone-200 pl-3 placeholder:text-stone-400"
+              className="bg-black border-b-[1px] border-stone-400 outline-none w-full pb-2 text-stone-300  placeholder:text-stone-400 placeholder:pl-3"
               type="text"
-              placeholder="Your e-mail"
-            />
-            <input
-              className="bg-black border-b-[1px] border-stone-400 outline-none w-full pb-2 text-stone-200 pl-3 placeholder:text-stone-400"
-              type="text"
-              placeholder="Your e-mail"
+              value={productInfo.image}
+              onChange={(e) => {
+                setProductInfo({
+                  ...productInfo,
+                  image: e.target.value,
+                });
+              }}
             />
           </div>
+          <Button>Add Product</Button>
         </form>
       </div>
     </div>
