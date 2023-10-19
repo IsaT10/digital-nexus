b6561e091702184812b8ed2f0d9ad360791fe3b9
IsaT <rakiib010@gmail.com>
2023-10-19 07:09:19 +0600

    specific brand products add and get specific brand name products only

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const Products = () => {
-  return <div>asadas</div>;
+  const [brandProducts, setBrandProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  const { brand } = useParams();
+  console.log(brand);
+  console.log(brandProducts);
+  useEffect(() => {
+    const searchProducts = async () => {
+      try {
+        const response = await fetch(`http://localhost:5000/products/${brand}`);
+        const data = await response.json();
+        setBrandProducts(data);
+        setLoading(false);
+        setError("");
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+        setError("Failed to fetch data");
+      }
+    };
+    searchProducts();
+  }, [brand]);
+  return <div>{brandProducts.length}</div>;
 };
 
 export default Products;
